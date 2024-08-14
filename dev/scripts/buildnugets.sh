33c920398cf00217caa6ea1ae13c67002977cd0f
Calin <calin.dobos@qwebsolutions.eu>
2024-08-14 22:32:06 +0300

    Dev solution based on nugets.

---
+++ dev/scripts/buildnugets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+frameworkRoot=c:/github/qwebsolutions/metapsi
+outputFolder=../nugets
+
+REPO_COMMIT=$(git rev-parse HEAD)
+echo $REPO_COMMIT > ../metapsi_version.txt
+
+rm ../nugets -rf
+
+echo "Output folder: $outputFolder"
+
+source ./projects.sh
+for p in ${projects[@]}
+do
+	dotnet pack $frameworkRoot/$p -o $outputFolder -c Debug -p:Version="0.0.0-dev"
+done
