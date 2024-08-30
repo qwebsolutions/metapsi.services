776d57f76144b440474c6b136a917f919d50db66
Calin <calin.dobos@qwebsolutions.eu>
2024-08-30 17:58:21 +0300

    nuget publish script

---
+++ Scripts/pack.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+    echo 'Version not specified'
+    exit 1
+else
+    echo 'Packing version '$1
+fi
+
+REPO_URL=$(git config --get remote.origin.url)
+REPO_BRANCH=$(git branch --show-current)
+REPO_COMMIT=$(git rev-parse HEAD)
+
+rm ./nugets -rf
+version=$1
+outputFolder=nugets
+echo "Output folder: $outputFolder"
+echo "Version: $version"
+dotnet pack ../Metapsi.Chat -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
+dotnet pack ../Metapsi.ServiceDoc -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
+dotnet pack ../Metapsi.SignIn -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
+dotnet pack ../Metapsi.WhatsApp -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
